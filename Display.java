00fc115255d7c4e043178756a47462d2a430631f
Mongkol88 <flucktv25@gmail.com>
2026-01-31 16:55:42 +0700

    Fix 0.1

--- Display.java
+++ Display.java
@@ -2,10 +2,7 @@ import java.util.Scanner;
 
 public class Display {
     private double result = 0;
-
-    public Display(double initresult) {
-        this.result = initresult;
-    }
+    
     public void Interface() {
         System.out.println("Enter 0-9 to Add Number");
         System.out.println("Enter (+ - * / %) to Operator ");
